f57f11c05fd94536988d45bdaf2a300b57664a93
Delphinedg <adelphineduong@gmail.com>
2021-09-20 16:30:31 +0200

    Mise à jour de la validation des champs du formulaire

--- routes/sauce.js
+++ routes/sauce.js
@@ -3,12 +3,11 @@ const router = express.Router();
 const sauceCrtl = require("../controllers/sauce");
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
-const validateSauce = require("../middleware/sauceValidator");
 
 router.get("/", auth, sauceCrtl.getAllSauces);
-router.post("/", auth, multer, validateSauce, sauceCrtl.createSauce);
+router.post("/", auth, multer, sauceCrtl.createSauce);
 router.get("/:id", auth, sauceCrtl.getOneSauce);
-router.put("/:id", auth, multer, validateSauce, sauceCrtl.updateSauce);
+router.put("/:id", auth, multer, sauceCrtl.updateSauce);
 router.delete("/:id", auth, sauceCrtl.deleteSauce);
 router.post("/:id/like", auth, sauceCrtl.likeSauce);
 
